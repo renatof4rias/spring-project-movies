aaae295e7737ce2f2a6a45acf30db305305b35ab
renatof4rias <renatof4rias@gmail.com>
2024-01-16 17:30:52 -0300

    DB SEED Entity USER

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_user(email) VALUES ('maria@gmail.com');
+INSERT INTO tb_user(email) VALUES ('joao@gmail.com');
+INSERT INTO tb_user(email) VALUES ('ana@gmail.com');
+INSERT INTO tb_user(email) VALUES ('lucia@gmail.com');
+INSERT INTO tb_user(email) VALUES ('joaquim@gmail.com');
